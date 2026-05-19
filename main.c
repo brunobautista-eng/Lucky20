cd5f67a6937a7bfc529f867f05aab87e71b09665
brunobautista-eng <bruno.bautista@uabc.edu.mx>
2026-05-18 21:03:19 -0700

    Menu

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int opcion;
+    do {
+        printf("1. Nueva partida \n 2. Ver ganadores \n 3. Reproducir partida \n");
+        scanf("%d",&opcion);
+    }while (opcion != 4);
+
     return 0;
-
-    //esto es una prueba44554
-
-    //222222
 }
