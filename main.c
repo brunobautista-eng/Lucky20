267cfcc9f9f0f9f1eee4b039ad47a225941f1d93
brunobautista-eng <bruno.bautista@uabc.edu.mx>
2026-05-18 21:07:58 -0700

    Menu esquema final

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
+    //menu principal, utiliza switch case
     int opcion;
     do {
         printf("1. Nueva partida \n 2. Ver ganadores \n 3. Reproducir partida \n");
         scanf("%d",&opcion);
+        switch (opcion) {
+            case 1:
+                //funcion para iniciar juego
+                break;
+            case 2: break;
+                //funcion para ver lista en archivo
+            case 3: break;
+                //funcion para leer archivo
+            case 4: printf("Fin de programa\n");
+        }
+
     }while (opcion != 4);
 
     return 0;
